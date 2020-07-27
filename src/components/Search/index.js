adaedace39d8a2019b149a49668da4d405943c36
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-27 14:35:09 -0300

    put the field setting inside the hooks

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,77 +1,92 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {Platform} from 'react-native';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 
 function Search() {
+  const query = useMemo(
+    () => ({
+      key: 'AIzaSyCLdoVS9hgWcjWXjS9BzyNN2LM3HT1n3G4',
+      language: 'pt',
+    }),
+    [],
+  );
+
+  const textInputProps = useMemo(
+    () => ({
+      autoCapitalize: 'none',
+      autoCorrect: false,
+    }),
+    [],
+  );
+
+  const style = useMemo(
+    () => ({
+      container: {
+        position: 'absolute',
+        top: Platform.select({ios: 60, android: 40}),
+        width: '100%',
+      },
+      textInputContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        height: 54,
+        marginHorizontal: 20,
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+      },
+      textInput: {
+        height: 54,
+        margin: 0,
+        borderRadius: 0,
+        paddingTop: 0,
+        paddingBottom: 0,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginTop: 0,
+        marginLeft: 0,
+        marginRight: 0,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: {x: 0, y: 0},
+        shadowRadius: 15,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        fontSize: 18,
+      },
+      listView: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        backgroundColor: '#fff',
+        marginHorizontal: 20,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: {x: 0, y: 0},
+        shadowRadius: 15,
+        marginTop: 10,
+      },
+      description: {
+        fontSize: 16,
+      },
+      row: {
+        padding: 10,
+        heigth: 58,
+      },
+    }),
+    [],
+  );
+
   return (
     <GooglePlacesAutocomplete
       placeholder="Para onde?"
       placeholderTextColor="#333"
       onPress={() => {}}
-      query={{
-        key: 'AIzaSyCLdoVS9hgWcjWXjS9BzyNN2LM3HT1n3G4',
-        language: 'pt',
-      }}
-      textInputProps={{
-        autoCapitalize: 'none',
-        autoCorrect: false,
-      }}
+      query={query}
+      textInputProps={textInputProps}
       fetchDetails
       enablePoweredByContainer={false}
-      styles={{
-        container: {
-          position: 'absolute',
-          top: Platform.select({ios: 60, android: 40}),
-          width: '100%',
-        },
-        textInputContainer: {
-          flex: 1,
-          backgroundColor: 'transparent',
-          height: 54,
-          marginHorizontal: 20,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-        },
-        textInput: {
-          height: 54,
-          margin: 0,
-          borderRadius: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          paddingLeft: 20,
-          paddingRight: 20,
-          marginTop: 0,
-          marginLeft: 0,
-          marginRight: 0,
-          elevation: 5,
-          shadowColor: '#000',
-          shadowOpacity: 0.1,
-          shadowOffset: {x: 0, y: 0},
-          shadowRadius: 15,
-          borderWidth: 1,
-          borderColor: '#ddd',
-          fontSize: 18,
-        },
-        listView: {
-          borderWidth: 1,
-          borderColor: '#ddd',
-          backgroundColor: '#fff',
-          marginHorizontal: 20,
-          elevation: 5,
-          shadowColor: '#000',
-          shadowOpacity: 0.1,
-          shadowOffset: {x: 0, y: 0},
-          shadowRadius: 15,
-          marginTop: 10,
-        },
-        description: {
-          fontSize: 16,
-        },
-        row: {
-          padding: 10,
-          heigth: 58,
-        },
-      }}
+      styles={style}
     />
   );
 }
